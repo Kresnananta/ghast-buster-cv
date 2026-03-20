e7289d0e2dca0ad9b5cf8ee0b0aa949aea65cd92
Kresnananta <akresnaananta@gmail.com>
2026-03-20 20:11:30 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ghast Buster
+
+A computer vision-based mini-game inspired by Minecraft mechanics, built entirely using **OpenCV** and **NumPy**.
---
+++ main.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+
+cap = cv.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    cv.imshow('Camera Feed', frame)
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
