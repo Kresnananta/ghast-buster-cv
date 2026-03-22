aba9a0c6f42b8ebadcd9d0dacca1a0fbbd1cd8ba
Kresnananta <akresnaananta@gmail.com>
2026-03-22 17:22:15 +0700

    added: calibrate cam feat, basic hand detection using HSV mask

---
+++ calibrateCam.py
@@ -0,0 +1,49 @@
+import cv2
+import numpy as np
+
+def nothing(x):
+    pass
+
+cap = cv2.VideoCapture(0)
+
+cv2.namedWindow("Trackbars")
+
+# H = Hue, S = Saturation, V = Value
+# Lower - Upper => Threshold supaya tangan kedetect
+cv2.createTrackbar("L - H", "Trackbars", 0, 179, nothing)
+cv2.createTrackbar("L - S", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("L - V", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("U - H", "Trackbars", 179, 179, nothing)
+cv2.createTrackbar("U - S", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    
+    # mirroring frame
+    frame = cv2.flip(frame, 1)
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) # BGR -> HSV
+
+    l_h = cv2.getTrackbarPos("L - H", "Trackbars")
+    l_s = cv2.getTrackbarPos("L - S", "Trackbars")
+    l_v = cv2.getTrackbarPos("L - V", "Trackbars")
+    u_h = cv2.getTrackbarPos("U - H", "Trackbars")
+    u_s = cv2.getTrackbarPos("U - S", "Trackbars")
+    u_v = cv2.getTrackbarPos("U - V", "Trackbars")
+
+    lower_skin = np.array([l_h, l_s, l_v])
+    upper_skin = np.array([u_h, u_s, u_v])
+
+    mask = cv2.inRange(hsv, lower_skin, upper_skin)
+
+    cv2.imshow("Camera", frame)
+    cv2.imshow("Mask Kalibrasi Camera", mask)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,22 +1,51 @@
-import cv2 as cv
+import cv2
+import numpy as np
 
-cap = cv.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
+lower_skin = np.array([0, 23, 141])
+upper_skin = np.array([89, 255, 255])
 
 while True:
     ret, frame = cap.read()
-
     if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
+        print("Can't receive frame")
         break
 
-    cv.imshow('Camera Feed', frame)
+    frame = cv2.flip(frame, 1)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    if cv.waitKey(1) & 0xFF == ord('q'):
+    mask = cv2.inRange(hsv, lower_skin, upper_skin) # threshold 
+
+    kernel = np.ones((5, 5), np.uint8)
+    mask = cv2.erode(mask, kernel, iterations=1) # mengikis noise kecil
+    mask = cv2.dilate(mask, kernel, iterations=2) # menebalkan tangan kembali
+
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    if contours:
+        # ambil contour yg paling luas (meminimalisir wajah terdetek)
+        largest_contour = max(contours, key=cv2.contourArea)
+        if cv2.contourArea(largest_contour) > 1000:
+            # cari titik tengah
+            M = cv2.moments(largest_contour)
+            if M["m00"] != 0:
+                cx = int(M["m10"] / M["m00"])
+                cy = int(M["m01"] / M["m00"])
+                # m00 = total luas area putih; m10 & m01 = jumlah posisi pixel
+
+                cv2.circle(frame, (cx, cy), 50, (0, 255, 255), 3)
+                cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
+                
+                cv2.drawContours(frame, [largest_contour], -1, (0, 255, 0), 2)
+
+    cv2.imshow("Shield Defense", frame)
+    cv2.imshow("Clean Mask", mask)
+
+    cv2.imshow('Camera Feed', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
